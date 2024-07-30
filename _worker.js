daad0dc9923324138ca056598a73a9b02412f817
hozhao <66132675+hozhao@users.noreply.github.com>
2024-07-30 17:34:01 +0800

    Update2 _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ea4d343c-b7ab-4cf0-84a7-386c242475a0';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
